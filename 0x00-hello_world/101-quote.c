2917a4c0a8cb6c0fbe8ce7352038d81c1fd544d6
Angela <angelaribeiro06@gmail.com>
2023-02-21 05:53:34 -0800

    Task0_putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,15 @@
 #include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+#include <unistd.h>
 
 int main(void)
 {
-  char *message = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, message, 47);
-  return 1;
+	const char message[] = "and that peice of art is useful\" - "
+		"Dora Korpar,2015-10-19\n"
+		writr(STDERR_FILENO, message, sizeof(message) - 1);
+	return (1);
 }
-
