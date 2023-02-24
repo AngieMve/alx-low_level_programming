4e08b717bd7ab2950db021dcd27e210eaa22d1b9
Angela <angelaribeiro06@gmail.com>
2023-02-24 05:12:48 -0800

    Task 9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,26 +8,34 @@
   */
 int main(void)
 {
-	int i;
+	int x;
 
-	for (i = 1; i <= 100; i++)
+	for (x = 1; x <= 100; x++)
 	{
-	if (i % 3 == 0 && i % 5 == 0)
-	{
-	printf("FizzBuzz ");
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+
+		if (x != 100)
+		{
+			printf(" ");
+		}
 	}
-	else if (i % 3 == 0)
-	{
-	printf("Fizz ");
-	}
-	else if (i % 5 == 0)
-	{
-	printf("Buzz ");
-	}
-	else
-	{
-	printf("%d ", i);
-	}
-	}
+
+	printf("\n");
+
 	return (0);
 }
