87460a922d7405a95ef43de0889fc507aa6a11eb
Angela <angelaribeiro06@gmail.com>
2023-02-27 12:35:25 -0800

    Task 2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * main - check the code
   * _strcpy - Copy a string
-  * @dest: Destination value
-  * @src: Source value
   *
   * Return: the pointer to dest
   */
