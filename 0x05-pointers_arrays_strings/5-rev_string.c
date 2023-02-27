8ac7addeb085a94fc4b44fbc0fd1f6b084359691
Angela <angelaribeiro06@gmail.com>
2023-02-27 11:58:38 -0800

    Task5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,34 +1,45 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
-  * rev_string - Reverses a string
-  * @s: IThe string to be modified
-  *
-  * Return: void
-  */
+ * rev_string - reverse a string
+ * @s: the parameter to be reversed
+ * Return: the revesed string
+ */
+
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+	int i;
+	int len;
+	char *rev = s;
+	char tmp;
 
-	a = s;
+	len = _strlen(s);
+	while (*(rev++) != '\0')
+	continue;
+	rev = rev - 2;
 
-	while (s[c] != '\0')
+	for (i = 0; i < len / 2; ++i)
 	{
-		c++;
-	}
+	tmp = s[i];
+	s[i] = rev[0];
+	rev[0] = tmp;
+	rev--;
 
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
 }
+}
+/**
+  * _strlen - return the lenght
+  * @s: the parameter
+  * Return: the length of an array
+  */
 
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+
+}
